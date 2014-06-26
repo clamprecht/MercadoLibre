0380e1e3d077938b7be545ff4e96d0d702e7c237
Chris Lamprecht <clamprecht@gmail.com>
2014-06-26 19:08:14 -0300

    First attempt, O(nlogn)

--- Problema.java
+++ Problema.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Problema {
@@ -12,37 +13,59 @@ public class Problema {
 		Integer result = mayorRepetido(numeros);
 		
 		System.out.println("Mayor Repetido: "+ result);
-		
-	}
+
+        // test cases
+        System.out.println("42: " + mayorRepetido(new LinkedList<Integer>(Arrays.asList(42))));
+        System.out.println("41: " + mayorRepetido(new LinkedList<Integer>(Arrays.asList(42,41))));
+        System.out.println("1: " + mayorRepetido(new LinkedList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1))));
+        System.out.println("4: " + mayorRepetido(new LinkedList<Integer>(Arrays.asList(9,8,7,6,5,4,4,3,2,1))));
+        System.out.println("1: " + mayorRepetido(new LinkedList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1,1))));
+        System.out.println("9: " + mayorRepetido(new LinkedList<Integer>(Arrays.asList(9,9,8,7,6,5,4,3,2,1))));
+        try {
+            // test degenerate case
+            mayorRepetido(new LinkedList<Integer>());
+            System.out.println("Failed test for empty list");
+        } catch (IllegalArgumentException e) {
+            // test passed
+        }
+    }
 
 	private static Integer mayorRepetido(LinkedList<Integer> numeros) {
+        if (numeros.isEmpty()) {
+            // evitar caso degenerado
+            throw new IllegalArgumentException("Empty list not allowed");
+        }
 
-		//En esta variable me voy quedando con el más repetido
-		Integer repetido=0;
-		
-		//En esta variable me quedo con la cantidad de repeticiones
-		Integer repeticiones=0;
-		
-		//Por cada número en la lista, recorro la lista y cuento cuantos números son iguales
-		for (Integer num : numeros) {
-	
-			int count=0;
-			for (Integer num2 : numeros) {			
-				//si el numero es igual cuento una repetición
-				if(num==num2)
-					count++;
-			}
-			
-			//si las repeticiones son mayores a las que tenía contadas, me lo guardo
-			if (count>repeticiones) {
-				repeticiones=count;
-				repetido=num;
-			}
-			
-		}
-			
-		//Devuelvo el más repetido
-		return repetido;
+        //Sortear primero, complejidad O(nlogn), incluso con LinkedList
+        Collections.sort(numeros);
+
+        int current = numeros.get(0);
+
+        //En esta variable me voy quedando con el más repetido
+        int repetido=numeros.get(0);
+
+        //En esta variable me quedo con la cantidad de repeticiones
+        int repeticiones=1, max = 1;
+
+        for (int i = 1; i < numeros.size(); i++) {
+            int element = numeros.get(i);
+
+            if (element == current) {
+                // sigue contando current numero
+                repeticiones++;
+                if (repeticiones > max) {
+                    max = repeticiones;
+                    repetido = current;
+                }
+            } else {
+                // nuevo valor
+                current = element;
+                repeticiones = 1;
+            }
+        }
+
+        //Devuelvo el más repetido
+        return repetido;
 	}
 	
 }
